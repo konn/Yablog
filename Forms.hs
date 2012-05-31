cc4f1692a39ca49f62d041ff9ba2120280b50276
Hiromi Ishii <konn.jinro@gmail.com>
2012-05-31 23:19:14 +0900

    some changes

--- Forms.hs
+++ Forms.hs
@@ -31,6 +31,7 @@ articleForm' mart mtags htm = do
       permissionDenied "You are not in admins"
   now  <- liftIO getCurrentTime
   askFiles >>= liftIO . print
+  askEnv >>= liftIO . print
   markup <- extraMarkup . appExtra . settings <$> lift getYesod
   ident <- maybe (lift newIdent) return $ articleIdent <$> mart
   let day  = utctDay now
